a13a31f7f21dc7327234f875bea6fb20d9b6d7ef
bogdan.hlyan <es.bogdan.hlyan@gmail.com>
2021-09-30 19:02:46 +0300

    fix proxy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -3,9 +3,9 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 const app = express()
 
 
-app.use('/ws', createProxyMiddleware({target: 'https://namisushi.dn.ua', changeOrigin: true, ws: true}))
-app.use('/api', createProxyMiddleware({target: 'https://namisushi.dn.ua', changeOrigin: true}))
-app.use('/image', createProxyMiddleware({target: 'https://namisushi.dn.ua', changeOrigin: true}))
+app.use('/ws', createProxyMiddleware({target: 'https://dev.namisushi.dn.ua', changeOrigin: true, ws: true}))
+app.use('/api', createProxyMiddleware({target: 'https://dev.namisushi.dn.ua', changeOrigin: true}))
+app.use('/image', createProxyMiddleware({target: 'https://dev.namisushi.dn.ua', changeOrigin: true}))
 app.use('/', createProxyMiddleware({target: 'http://localhost:8080', changeOrigin: true}))
 
 
